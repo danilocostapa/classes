82ea89ec3e7b4adaba21c27b7e4740264bbe0183
Danilo Costa <bass.danilo@gmail.com>
2022-05-04 22:17:50 -0300

    Adicionando metodo que calcula a média

--- classes/src/executavel/Executavel.java
+++ classes/src/executavel/Executavel.java
@@ -5,5 +5,13 @@ import classes.Aluno;
 public class Executavel {
 	public static void main(String[] args) {
 		Aluno aluno1 = new Aluno();
+		
+		aluno1.setNota1(90);
+		aluno1.setNota2(86.6);
+		aluno1.setNota3(66.8);
+		aluno1.setNota4(88.7);
+		
+		System.out.println("Média: " + aluno1.getMediaNota());
+		
 	}
 }
