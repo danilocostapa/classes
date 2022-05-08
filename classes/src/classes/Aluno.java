8b7425ac4d37ee4c59d4312f5abbf39ad2a51b92
Danilo Costa <bass.danilo@gmail.com>
2022-05-08 20:53:16 -0300

    Adicionando lista de Disciplinas

--- classes/src/classes/Aluno.java
+++ classes/src/classes/Aluno.java
@@ -13,11 +13,10 @@ public class Aluno {
 	private String dataMatricula;
 	private String nomeEscola;
 	private String serieMatriculado;
+	
+	private Disciplina disciplina = new Disciplina(); //relacionando a disciplina com o  aluno
 
-	private double nota1;
-	private double nota2;
-	private double nota3;
-	private double nota4;
+	
 
 	/* Método construtor padrão */
 	public Aluno() {
@@ -26,7 +25,7 @@ public class Aluno {
 
 	/* Construtor com argumentos */
 	public Aluno(String nome, int idade, String dataNascimento, String registroGeral, String numeroCpf, String nomeMae,
-			String nomePai, String dataMatricula, String nomeEscola, String serieMatriculado, double nota1, double nota2, double nota3, double nota4) {
+			String nomePai, String dataMatricula, String nomeEscola, String serieMatriculado) {
 		this.nome = nome;
 		this.idade = idade;
 		this.dataNascimento = dataNascimento;
@@ -37,10 +36,7 @@ public class Aluno {
 		this.dataMatricula = dataMatricula;
 		this.nomeEscola = nomeEscola;
 		this.serieMatriculado = serieMatriculado;
-		this.nota1 = nota1;
-		this.nota2 = nota2;
-		this.nota3 = nota3;
-		this.nota4 = nota4;
+		
 	}
 
 	/*
@@ -127,77 +123,83 @@ public class Aluno {
 	public void setSerieMatriculado(String serieMatriculado) {
 		this.serieMatriculado = serieMatriculado;
 	}
-
-	public double getNota1() {
-		return nota1;
+	
+	public void setDisciplina(Disciplina disciplina) {
+		this.disciplina = disciplina;
+	}
+	
+	public Disciplina getDisciplina() {
+		return disciplina;
 	}
 
-	public void setNota1(double nota1) {
-		this.nota1 = nota1;
-	}
-
-	public double getNota2() {
-		return nota2;
-	}
-
-	public void setNota2(double nota2) {
-		this.nota2 = nota2;
-	}
-
-	public double getNota3() {
-		return nota3;
-	}
-
-	public void setNota3(double nota3) {
-		this.nota3 = nota3;
-	}
-
-	public double getNota4() {
-		return nota4;
-	}
-
-	public void setNota4(double nota4) {
-		this.nota4 = nota4;
-	}
+	
 
 	/* métodos */
 	/* Retorna a média do Aluno */
 	public double getMediaNota() {
-		double media = (nota1 + nota2 + nota3 + nota4) / 4;
+		double media = (disciplina.getNota1() + disciplina.getNota2() + disciplina.getNota3() + disciplina.getNota4()) / 4;
 		return media;
 	}
-	
-	/*método para saber se ta aprovado ou não*/
+
+	/* método para saber se ta aprovado ou não */
 	public boolean getAlunoAprovado() {
-		double media = this.getMediaNota();//atribuindo ao valor da média o resultado do método
-		
-		if(media >= 70) {
+		double media = this.getMediaNota();// atribuindo ao valor da média o resultado do método
+
+		if (media >= 70) {
 			return true;
-		}
-		else {
+		} else {
 			return false;
 		}
 	}
-	
+
 	public String getAlunoAprovado2() {
 		double media = this.getMediaNota();
-		
-		if(media >= 70) {
+
+		if (media >= 70) {
 			return "Aluno está aprovado";
-		}
-		else {
+		} else {
 			return "Aluno está reprovado";
 		}
 	}
 
 	@Override
 	public String toString() {
-		return "Aluno: \nNome: " + getNome() + ", \nIdade: " + getIdade() + ", \nData de Nascimento: " + getDataNascimento() + ", \nRegistro Geral: "
-				+ getRegistroGeral() + ", \nNumero do Cpf: " + getNumeroCpf() + ", \nNome da Mãe: " + getNomeMae() + ", \nNome do Pai: " + getNomePai()
-				+ ", \nData Matricula: " + getDataMatricula() + ", \nNome da Escola: " + getNomeEscola() + ", \nSerie Matriculado: "
-				+ getSerieMatriculado() + ", \nNota 1: " + getNota1() + ", \nNota 2: " + getNota2() + ", \nNota 3: " + getNota3() + ", \nNota 4: " + getNota4();
+		return "Aluno: \nNome: " + getNome() + " \nIdade: " + getIdade() + " \nData de Nascimento: "
+				+ getDataNascimento() + " \nRegistro Geral: " + getRegistroGeral() + " \nNumero do Cpf: "
+				+ getNumeroCpf() + " \nNome da Mãe: " + getNomeMae() + " \nNome do Pai: " + getNomePai()
+				+ " \nData Matricula: " + getDataMatricula() + " \nNome da Escola: " + getNomeEscola()
+				+ " \nSerie Matriculado: " + getSerieMatriculado() + "\nDisciplinas: " + getDisciplina();
 	}
-	
-	
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((nome == null) ? 0 : nome.hashCode());
+		result = prime * result + ((numeroCpf == null) ? 0 : numeroCpf.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Aluno other = (Aluno) obj;
+		if (nome == null) {
+			if (other.nome != null)
+				return false;
+		} else if (!nome.equals(other.nome))
+			return false;
+		if (numeroCpf == null) {
+			if (other.numeroCpf != null)
+				return false;
+		} else if (!numeroCpf.equals(other.numeroCpf))
+			return false;
+		return true;
+	}
 
 }
--- classes/src/executavel/Executavel.java
+++ classes/src/executavel/Executavel.java
@@ -1,12 +1,14 @@
 package executavel;
 
+import java.util.Dictionary;
+
 import javax.swing.JOptionPane;
 
 import classes.Aluno;
 
 public class Executavel {
 	public static void main(String[] args) {
-		
+
 		String nome = JOptionPane.showInputDialog("Qual o nome do aluno?");
 		int idade = Integer.parseInt(JOptionPane.showInputDialog("Qual a idade do aluno?"));
 		String dataNascimento = JOptionPane.showInputDialog("Data de Nascimento: dd/MM/yyyy");
@@ -17,15 +19,21 @@ public class Executavel {
 		String dataMatricula = JOptionPane.showInputDialog("Qual a data da Matricula? dd/MM/yyyy");
 		String nomeEscola = JOptionPane.showInputDialog("Qual o nome da Escola?");
 		String serieMatriculado = JOptionPane.showInputDialog("Qual série o aluno faz?");
-		
-		double nota1 = Double.parseDouble(JOptionPane.showInputDialog("Nota 1: "));
-		double nota2 = Double.parseDouble(JOptionPane.showInputDialog("Nota 2: "));
-		double nota3 = Double.parseDouble(JOptionPane.showInputDialog("Nota 3: "));
-		double nota4 = Double.parseDouble(JOptionPane.showInputDialog("Nota 4: "));
-		
-		Aluno aluno1 = new Aluno(nome, idade, dataNascimento, rg, cpf, nomeMae, nomePai, dataMatricula, nomeEscola, serieMatriculado, nota1, nota2, nota3, nota4);
-		/*Aluno aluno1 = new Aluno();
-		
+
+		String disciplina1 = JOptionPane.showInputDialog("Qual a disciplina 1?");
+		double nota1 = Double.parseDouble(JOptionPane.showInputDialog("Nota da disciplina " + disciplina1));
+
+		String disciplina2 = JOptionPane.showInputDialog("Qual a disciplina 2?");
+		double nota2 = Double.parseDouble(JOptionPane.showInputDialog("Nota da disciplina " + disciplina2));
+
+		String disciplina3 = JOptionPane.showInputDialog("Qual a disciplina 3?");
+		double nota3 = Double.parseDouble(JOptionPane.showInputDialog("Nota da disciplina " + disciplina3));
+
+		String disciplina4 = JOptionPane.showInputDialog("Qual a disciplina 4?");
+		double nota4 = Double.parseDouble(JOptionPane.showInputDialog("Nota da disciplina " + disciplina4));
+
+		Aluno aluno1 = new Aluno();
+
 		aluno1.setNome(nome);
 		aluno1.setIdade(idade);
 		aluno1.setDataNascimento(dataNascimento);
@@ -36,17 +44,23 @@ public class Executavel {
 		aluno1.setDataMatricula(dataMatricula);
 		aluno1.setNomeEscola(nomeEscola);
 		aluno1.setSerieMatriculado(serieMatriculado);
+
+		aluno1.getDisciplina().setNota1(nota1);
+		aluno1.getDisciplina().setNota2(nota2);
+		aluno1.getDisciplina().setNota3(nota3);
+		aluno1.getDisciplina().setNota4(nota4);
 		
+		aluno1.getDisciplina().setDisciplina1(disciplina1);
+		aluno1.getDisciplina().setDisciplina2(disciplina2);
+		aluno1.getDisciplina().setDisciplina3(disciplina3);
+		aluno1.getDisciplina().setDisciplina4(disciplina4);
+
 		
-		aluno1.setNota1(nota1);
-		aluno1.setNota2(nota2);
-		aluno1.setNota3(nota3);
-		aluno1.setNota4(nota4);*/
-		
+
 		System.out.println(aluno1);
-		
-		System.out.println("Média do Aluno: "+ aluno1.getMediaNota()+ ""
-				+ "\nResultado: " + (aluno1.getAlunoAprovado()? "Aprovado" : "Reprovado")); //operador ternário
-		
+
+		System.out.println("Média do Aluno: " + aluno1.getMediaNota() + "" + "\nResultado: "
+				+ (aluno1.getAlunoAprovado() ? "Aprovado" : "Reprovado")); // operador ternário
+
 	}
 }
